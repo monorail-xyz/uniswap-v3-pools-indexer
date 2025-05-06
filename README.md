8bd69a5ed32758544272cf2800cc49867e21dea6
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2025-05-06 23:40:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Uniswap V3 Pool Indexer
+
+This indexer is based on the information provided by the Envio team available [here](https://docs.envio.dev/docs/HyperIndex/contract-state).
+
+The implementation here is to capture only the information needed by Monorail.
+
+## Building
+
+```shell
+npm install
+npm run codegen
+npm run dev
+```
---
+++ src/EventHandlers.ts
@@ -0,0 +1,19 @@
+import {
+  V3Pools,
+  Pool,
+} from "generated";
+
+V3Pools.PoolCreated.handler(async ({ event, context }) => {
+
+  const entity: Pool = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    factory: event.srcAddress.toLowerCase(),
+    pool: event.params.pool.toLowerCase(),
+    token0: event.params.token0.toLowerCase(),
+    token1: event.params.token1.toLowerCase(),
+    fee: event.params.fee,
+    tickSpacing: event.params.tickSpacing,
+  };
+  context.Pool.set(entity);
+
+}, { wildcard: true });
