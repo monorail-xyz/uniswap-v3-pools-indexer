c329f9fa7ecc076cce87154dffd16e5ae27b07f1
Donovan Solms <4567303+donovansolms@users.noreply.github.com>
2025-08-24 09:55:58 +0800

    Update creation fields

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -7,6 +7,8 @@ V3Pools.PoolCreated.handler(async ({ event, context }) => {
 
   const entity: Pool = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    tx: event.transaction.hash,
+    created_via: event.transaction.to?.toLowerCase() || "",
     factory: event.srcAddress.toLowerCase(),
     pool: event.params.pool.toLowerCase(),
     token0: event.params.token0.toLowerCase(),
